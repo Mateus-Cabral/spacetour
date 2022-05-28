73f61732d4f86717c09a5ec332a9066dbb6920a4
giovana-abreu <rm95896@fiap.com.br>
2022-05-28 15:53:48 -0300

    Validação por JavaScript adicionada

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const botaoEnviar = document.getElementById("envioContato")
+
+botaoEnviar.addEventListener("click", validaCampos)
+
+function validaCampos(){
+
+    const campos = document.querySelectorAll("input[type='text'],input[type='email'], input[type='date']")
+
+    for(let x = 0; x < campos.length ; x++){
+       
+            if(campos[x].value == ""){
+                alert("Por favor, preencha o campo " + campos[x].name + "!")
+                return                
+            }
+    }
+}
